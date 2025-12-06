73c3c6181acdc6852b6659b782de36f89a891aab
Marcus <marcus.ericsson_92@icloud.com>
2025-12-06 19:58:50 +0100

    setup 11 deploy rag serverless

--- video_alongs/11_deploy_rag_serverless/backend/rag.py
+++ video_alongs/11_deploy_rag_serverless/backend/rag.py
@@ -1,35 +1,29 @@
 from pydantic_ai import Agent
 from backend.data_models import RagResponse
-import lancedb 
-from backend.constanst import VECTOR_DB_PATH
+from backend.constants import VECTOR_DATABASE
+import lancedb
 
-vector_db = lancedb.connect(VECTOR_DB_PATH)
+vector_db = lancedb.connect(uri= VECTOR_DATABASE)
 
-rag_agent = Agent(
-    model= "google-gla:gemini-2.5-flash",
-    retries=2,
-    system_prompt=(
+rag_agent = Agent(model= "google-gla:gemini-2.5-flash", retries=2, system_prompt= (
         "You are an expert in rabbit races and knows how to distinguish between the rabbits",
         "Always answer based on the retrieved knowledge, but you can mix in your expertise to make the answer more coherent",
         "Don't hallucinate, rather say you can't answer it if the user prompts outside of the retrieved knowledge",
         "Make sure to keep the answer clear and concise, getting to the point directly, max 6 sentences",
         "Also describe which file you have used as source",
-    ),
-    output_type=RagResponse
-)
+), output_type= RagResponse)
 
 @rag_agent.tool_plain
-def retrieve_top_documents(query: str, top_result = 3) -> str:
+def retrieve_top_documents(query: str, k=3) -> str:
+    """ 
+    Use vector search to find the closest k matching documents to the query
     """
-    User vector search to find top_result closest matching documents to use as context to th query
-    """
-    
-    results = vector_db["articles"].search(query= query,).limit(top_result).to_list()
-    
+    result = vector_db["articles"].search(query=query).limit(k).to_list()
+    top_result = result[0]
     return f"""
-    Filename: {results[0]["filename"]},
-    Filepath: {results[0]["filepath"]},
-    Content: {results[0]["content"]}
+    Filename: {top_result["filename"]},
+    Filepath: {top_result["filepath"]},
+    Content: {top_result["content"]}
     """
-
-
+    
+    
